5d6624a4eedb7833547408977dd424da0ec3f39e
DESKTOP-RLITA2J\rakuk <rakuk4783@gmail.com>
2023-05-21 17:15:02 +0900

    up

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using Photon.Pun;
+using System.Collections.Generic;
+
+public class SpawnManager : MonoBehaviourPunCallbacks
+{
+    public Transform[] spawnPositons;
+    private List<Transform> availableSpawnPositions;
+
+    public GameObject playerPrefab;
+    private GameObject player;
+    public float respawnInterval = 5f;
+
+    private void Start()
+    {
+        // スポーンポイントをコピーして利用可能なスポーンポイントリストを作成
+        availableSpawnPositions = new List<Transform>(spawnPositons);
+
+        if (PhotonNetwork.IsConnected)
+        {
+            SpawnPlayer();
+        }
+    }
+
+    public Transform GetSpawnPoint()
+    {
+        // リストからランダムに選んで位置情報を返す
+        int spawnIndex = PhotonNetwork.LocalPlayer.ActorNumber % availableSpawnPositions.Count;
+        Transform chosenSpawnPoint = availableSpawnPositions[spawnIndex];
+
+        // 選んだスポーンポイントを利用可能リストから削除
+        availableSpawnPositions.RemoveAt(spawnIndex);
+
+        return chosenSpawnPoint;
+    }
+
+    public void SpawnPlayer()
+    {
+        Transform spawnPoint = GetSpawnPoint();
+        player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+    }
+}
