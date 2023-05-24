734de88392cf741e1499b3f65b1a0554b832c838
DESKTOP-RLITA2J\rakuk <rakuk4783@gmail.com>
2023-05-24 20:10:18 +0900

    up

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,42 +1,65 @@
 using UnityEngine;
 using Photon.Pun;
-using System.Collections.Generic;
 
-public class SpawnManager : MonoBehaviourPunCallbacks
+public class SpawnManager : MonoBehaviour
 {
+    //スポーンポイントオブジェクト格納配列
     public Transform[] spawnPositons;
-    private List<Transform> availableSpawnPositions;
 
-    public GameObject playerPrefab;
-    private GameObject player;
-    public float respawnInterval = 5f;
+
+    public GameObject playerPrefab;//生成オブジェクト
+    private GameObject player;//生成したプレイヤーを格納
+    public float respawnInterval = 5f;//復活までに要する時間
+
 
     private void Start()
     {
-        // スポーンポイントをコピーして利用可能なスポーンポイントリストを作成
-        availableSpawnPositions = new List<Transform>(spawnPositons);
+        //スポーンポイントオブジェクトをすべて非表示
+        foreach (var pos in spawnPositons)
+        {
+            pos.gameObject.SetActive(false);
+        }
 
+
+        //ネットワークにつながっているならスポーン
         if (PhotonNetwork.IsConnected)
         {
             SpawnPlayer();
         }
     }
 
+    /// <summary>
+    /// リスポーン地点をランダム取得する関数
+    /// </summary>
     public Transform GetSpawnPoint()
     {
-        // リストからランダムに選んで位置情報を返す
-        int spawnIndex = PhotonNetwork.LocalPlayer.ActorNumber % availableSpawnPositions.Count;
-        Transform chosenSpawnPoint = availableSpawnPositions[spawnIndex];
-
-        // 選んだスポーンポイントを利用可能リストから削除
-        availableSpawnPositions.RemoveAt(spawnIndex);
-
-        return chosenSpawnPoint;
+        //ランダムでスポーンポイント１つ選んで位置情報を返す
+        return spawnPositons[Random.Range(0, spawnPositons.Length)];
     }
 
+    //Playerを生成する
     public void SpawnPlayer()
     {
         Transform spawnPoint = GetSpawnPoint();
-        player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+
+        //ネットワーク上のプレイヤーを生成する：第一引数ではオブジェクトの名前が必要
+        player = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);//後で削除するときのために格納
+    }
+
+
+    /// <summary>
+    /// 撃たれて体力がなくなった時に呼ばれる
+    /// </summary>
+    public void Die()
+    {
+
+        if (player != null)
+        {
+            //5秒後にリスポーンさせる
+            Invoke("SpawnPlayer", 2f);
+        }
+        //playerをネットワーク上から削除
+        PhotonNetwork.Destroy(player);
+
     }
 }
