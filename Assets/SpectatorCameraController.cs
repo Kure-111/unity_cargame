a6386c8117a3d44301019c51b0294b8ec3868c86
DESKTOP-RLITA2J\rakuk <rakuk4783@gmail.com>
2023-05-28 01:42:37 +0900

    add  drift

--- Assets/SpectatorCameraController.cs
+++ Assets/SpectatorCameraController.cs
@@ -5,6 +5,7 @@ public class SpectatorCameraController : MonoBehaviourPun
 {
     public float movementSpeed = 10.0f;  // カメラの移動速度
     public float zoomSpeed = 1000f;  // ズームの速度
+    public Camera cam;
 
     void Start()
     {
@@ -14,25 +15,34 @@ public class SpectatorCameraController : MonoBehaviourPun
 
     void Update()
     {
-        // 現在のクライアントがマスタークライアントかどうかを確認
-        if (PhotonNetwork.IsMasterClient)
+
+        if (photonView.IsMine)
         {
-            // WASDキーでカメラの移動
-            if (Input.GetKey(KeyCode.W))
-                transform.position += transform.up * movementSpeed * Time.deltaTime;
+            cam.enabled = true;
+            if (PhotonNetwork.IsMasterClient)
+            {
+                // WASDキーでカメラの移動
+                if (Input.GetKey(KeyCode.W))
+                    transform.position += transform.up * movementSpeed * Time.deltaTime;
 
-            if (Input.GetKey(KeyCode.S))
-                transform.position -= transform.up * movementSpeed * Time.deltaTime;
+                if (Input.GetKey(KeyCode.S))
+                    transform.position -= transform.up * movementSpeed * Time.deltaTime;
 
-            if (Input.GetKey(KeyCode.A))
-                transform.position -= transform.right * movementSpeed * Time.deltaTime;
+                if (Input.GetKey(KeyCode.A))
+                    transform.position -= transform.right * movementSpeed * Time.deltaTime;
 
-            if (Input.GetKey(KeyCode.D))
-                transform.position += transform.right * movementSpeed * Time.deltaTime;
+                if (Input.GetKey(KeyCode.D))
+                    transform.position += transform.right * movementSpeed * Time.deltaTime;
 
-            // マウススクロールでズームイン/アウト
-            float scroll = Input.GetAxis("Mouse ScrollWheel");
-            transform.position += transform.forward * scroll * zoomSpeed * Time.deltaTime;
+                // マウススクロールでズームイン/アウト
+                float scroll = Input.GetAxis("Mouse ScrollWheel");
+                transform.position += transform.forward * scroll * zoomSpeed * Time.deltaTime;
+            }
         }
+        else
+        {
+            cam.enabled = false;
+        }
+
     }
 }
