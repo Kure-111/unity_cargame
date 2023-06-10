9bc88ac58b7a65998262a05c9643278f81ef4d95
DESKTOP-RLITA2J\rakuk <rakuk4783@gmail.com>
2023-06-10 16:58:16 +0900

    no message

--- Assets/Resources/missile/LaserScript.cs
+++ Assets/Resources/missile/LaserScript.cs
@@ -8,16 +8,24 @@ public class LaserScript : MonoBehaviour
     private LineRenderer lineRenderer; // レーザーを描画するためのLineRenderer
     public float range = 10f; // レーザーの射程
 
+    private PhotonView photonView; // PhotonViewコンポーネント
+
     void Start()
     {
         // LineRendererコンポーネントを取得します
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 2; // 頂点数を2に設定します
+
+        // PhotonViewコンポーネントを取得します
+        photonView = GetComponent<PhotonView>();
     }
 
     // Updateはフレームごとに呼び出されます
     void Update()
     {
+        // 自分のプレイヤーでない場合は処理をスキップします
+        if (!photonView.IsMine) return;
+
         // このオブジェクトから前方にレイを飛ばします
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.forward, out hit, range))
