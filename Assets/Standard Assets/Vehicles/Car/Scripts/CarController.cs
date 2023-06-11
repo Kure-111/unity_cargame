2bbd67834e930dc92bc7819755a6d4becac22319
呉竹櫂人 <kure.developer@gmail.com>
2023-06-11 17:53:40 +0900

    no message

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -36,7 +36,7 @@ namespace UnityStandardAssets.Vehicles.Car
         [SerializeField] private SpeedType m_SpeedType;
         [SerializeField] public float m_Topspeed = 200;
         [SerializeField] private static int NoOfGears = 5;
-        [SerializeField] private float m_RevRangeBoundary = 1f;
+        [SerializeField] public float m_RevRangeBoundary = 1f;
         [SerializeField] private float m_SlipLimit;
         [SerializeField] private float m_BrakeTorque;
 
